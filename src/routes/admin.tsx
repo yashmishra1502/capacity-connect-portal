9e9619b0c6b57021c33c08d270d47b80b5457276
yashmishra1502 <yashmishr1502@gmail.com>
2026-08-23 13:19:02 +0530

    Delete src/routes/admin.tsx

--- src/routes/admin.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { AppShell } from "@/components/app-shell";
-
-export const Route = createFileRoute("/admin")({
-  component: () => <AppShell role="admin" />,
-});
