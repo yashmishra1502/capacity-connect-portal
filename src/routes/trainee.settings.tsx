9a8373078fda5f4a37f7efa070172270ffa81479
yashmishra1502 <yashmishr1502@gmail.com>
2026-08-23 13:23:00 +0530

    Delete src/routes/trainee.settings.tsx

--- src/routes/trainee.settings.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { SettingsPage } from "@/components/settings-page";
-
-export const Route = createFileRoute("/trainee/settings")({
-  component: () => <SettingsPage role="trainee" />,
-});
