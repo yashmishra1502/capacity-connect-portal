2ec8eb5afb6c0c34fc788681241c7f34521effbd
yashmishra1502 <yashmishr1502@gmail.com>
2026-08-23 14:24:33 +0530

    Update trainee.tsx

--- src/routes/trainee.tsx
+++ src/routes/trainee.tsx
@@ -1,6 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { AppShell } from "@/components/app-shell";
+import { RequireRole } from "@/components/require-role";
 
 export const Route = createFileRoute("/trainee")({
-  component: () => <AppShell role="trainee" />,
+  component: () => (
+    <RequireRole role="trainee">
+      <AppShell role="trainee" />
+    </RequireRole>
+  ),
 });
