1dbf7a7cb599e4b7504e6f9395e3e380e2a140fa
yashmishra1502 <yashmishr1502@gmail.com>
2026-08-27 14:22:33 +0530

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -132,7 +132,6 @@ function RootComponent() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
       <Outlet />
       <ChatWidget />
     </QueryClientProvider>
